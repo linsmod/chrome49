7f0247f6a84866d47e487122f5f5f83a645d7e07
wahahahaha <tiny@sandsea.info>
2024-10-02 16:31:27 +0800

    Add comment words for how doing scheduleAnimation

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -4340,7 +4340,7 @@ void WebViewImpl::scheduleAnimation()
 {
     if (m_layerTreeView) {
         m_layerTreeView->setNeedsBeginFrame();
-        // return;
+        // return; //@ linsmod: Do not return here becase scheduleAnimation is needed.
     }
     if (m_client)
         m_client->scheduleAnimation();
